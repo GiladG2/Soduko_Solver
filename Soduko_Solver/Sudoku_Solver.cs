6c88d439f28ab46162137704d8239dfc7073e312
= <=>
2026-02-01 12:59:23 +0200

    refactor:Added helper class in order to refactor the Sudoku_Solver class

--- Soduko_Solver/Sudoku_Solver.cs
+++ Soduko_Solver/Sudoku_Solver.cs
@@ -6,7 +6,7 @@ using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using static Soduko_Solver.Sudoku_Solver_Helper;
 namespace Soduko_Solver
 {
     public class Sudoku_Solver
@@ -15,19 +15,7 @@ namespace Soduko_Solver
         public Sudoku_Solver(int[,] mat)
         {
             state = new SudokuState(mat);
-        }
-        static void ResetEmpties(int[,] mat)
-        {
-            //Intialize the empties list
-            state.Empties = new List<(int, int)>();
-            for (int rows = 0; rows < mat.GetLength(0); rows++)
-                for (int cols = 0; cols < mat.GetLength(0); cols++)
-                {
-                    if (state.Mat[rows, cols] + '0' < '0' || state.Mat[rows, cols] > state.Mat.GetLength(0))
-                        throw new Invalid_Character_Exception(state.Mat[rows, cols], rows + 1, cols + 1);
-                    if (state.Mat[rows, cols] == 0)
-                        state.Empties.Add((rows, cols));
-                }
+            Sudoku_Solver_Helper s = new Sudoku_Solver_Helper(state);
         }
 
         //func that recieves a Sudoku board and returns true if it is solvable and false if not
@@ -40,7 +28,7 @@ namespace Soduko_Solver
                     if (state.Mat[rows, cols] + '0' < '0' || state.Mat[rows, cols] > state.Mat.GetLength(0))
                         throw new Invalid_Character_Exception(state.Mat[rows, cols], rows + 1, cols + 1);
                     if (state.Mat[rows, cols] != 0)
-                        AddSeenDigits(state.Mat[rows, cols], rows, cols);
+                       AddSeenDigits(state.Mat[rows, cols], rows, cols);
                     else
                         state.Empties.Add((rows, cols));
                 }
@@ -84,17 +72,7 @@ namespace Soduko_Solver
                 SimpleBacktracking(target, j + 1);
             return false;
         }
-        //Function that returns a bit with all the possible values of cell (r,c)
-        private static int GetUsedValues(int r,int c)
-        {
-            int b = (r / state.BoxSize) * state.BoxSize + (c / state.BoxSize);
-            return state.BoxesBitMask[b] | state.RowsBitMask[r] | state.ColsBitMask[c];
-        }
-        //Function that returns the size of the cell's domain (amount of valid options)
-        public static int GetAmoutOfOptions(int used)
-        {
-            return state.Len - Count_Bits(used);
-        }
+        //Function that chains naked singles (implementing constraint propagation)
         private static bool ChainNakedSingles(SudokuStack s)
         {
             var (r, c) = (0, 0);
@@ -114,7 +92,7 @@ namespace Soduko_Solver
                     }//Failed branch - a cell's domain = 0, meaning no valid placements in an empty cell
                     if (options == 1) //A cell with one option - found a naked single
                     {
-                        propagation = true;
+                        propagation = true; // Found a naked single => Continue chaining
                         int bitmask = (~used) & ((1 << state.Len) - 1);
                         int pick = bitmask & (-bitmask);
                         int num = Get_Bit_Position(pick);
@@ -126,6 +104,7 @@ namespace Soduko_Solver
             while (propagation);
             return true;
         }
+        //Function that returns if it is effiecient to chain naked singles
         private static bool IsNakedSinglesEfficient()
         {
             return state.Len > 9 // If the mat is large enough
@@ -144,6 +123,7 @@ namespace Soduko_Solver
         {
             int[] domain = GetDomain(used);//Get cell (r,c)'s domain    
             LCV_Value[] lcvSortedArray = new LCV_Value[domain.Length];
+            //If there are not a lof of options LCV is not efficient, thus skipping it
             if (domain.Length < 3)
             {
                 for (int i = 0; i < domain.Length; i++)
@@ -177,7 +157,7 @@ namespace Soduko_Solver
                 for (int k = boxS; k < boxS + state.BoxSize; k++)
                     for (int j = boxF; j < boxF + state.BoxSize; j++)
                     {
-                        if (k == r || j == c )
+                        if (k == r || j == c ) // Already checked for row and column
                             continue;
                         if (state.Mat[k,j] == 0 && !Is_Already_Placed((GetUsedValues(k, j)), domain[i]))
                             currentOptionsRemoved++;
@@ -245,78 +225,8 @@ namespace Soduko_Solver
             return false;
         }
 
-        static void RollBack(int diff, int mode)
-        {
-            for (int j = 0; j < diff; j++)
-            {
-                var (number, index, (row, collumn)) = state.Stack.Pop();
-                RemoveSeenDigit(number, row, collumn);
-                state.Mat[row, collumn] = 0;
-                if ((mode == 0 && j != diff - 1) || mode == 1)   //If rolling back from naked singles => roll everything back
-                    state.Empties.Insert(index, (row, collumn)); //If not => do not roll the current guess.
 
-            }
-        }
-        //Function that adds the number k to the bitmasks of row, col and their box
-        static void AddSeenDigits(int k, int row, int col)
-        {
-            if (Is_Already_Placed(state.RowsBitMask[row], k))
-                throw new Duplicate_Val_In_Row(k, row + 1);
-            if (Is_Already_Placed(state.ColsBitMask[col], k))
-                throw new Duplicate_Val_In_Column(k, col + 1);
-            int b = (row / state.BoxSize) * state.BoxSize + (col / state.BoxSize);
-            if (Is_Already_Placed(state.BoxesBitMask[b], k))
-                throw new Duplicate_Val_In_Box(k, b + 1);
-            state.RowsBitMask[row] |= 1 << (k - 1);
-            state.ColsBitMask[col] |= 1 << (k - 1);
-            state.BoxesBitMask[b] |= 1 << (k - 1);
-        }
-        //Func that recives a bitmask and returns the position of the heighest turned on bit in the bitmask
-        static public int Get_Bit_Position(int mask)
-        {
-            int pos = 0;
-            while ((mask >> pos) > 1) pos++;
-            return pos + 1;
-        }
-        //Function that returns an array of the domain
-        public static int[] GetDomain(int used)
-        {
-            int[] domain = new int[GetAmoutOfOptions(used)];
-            int k = 0;
-            for (int j = 0; j < state.Len; j++)
-            {
-                if ((used & 1) == 0)
-                {
-                    domain[k] = j + 1;
-                    k++;
-                }
-                used >>= 1;
-            }
-            return domain;
-        }
-        //Function that removes the number k from the bitmasks of row, col and their box
-        static void RemoveSeenDigit(int k, int row, int col)
-        {
-            state.RowsBitMask[row] &= ~(1 << ((k - 1)));
-            state.ColsBitMask[col] &= ~(1 << ((k - 1)));
-            state.BoxesBitMask[(row / state.BoxSize) * state.BoxSize + (col / state.BoxSize)] &= ~(1 << ((k - 1)));
-        }
-        //recieves integer n and returns the number of 1 bits in it.
-        public static int Count_Bits(int n)
-        {
-            int count = 0;
-            while (n > 0)
-            {
-                n &= (n - 1);
-                count++;
-            }
-            return count;
-        }
-        //Function that checks if the nth bit is already turned on in bit
-        public static bool Is_Already_Placed(int bit, int n)
-        {
-            return (bit & (1 << (n - 1))) != 0;
-        }
+        
         //Commented due to making my solver slower
         /*
         private static int CalculateOptions(int r, int c)
---
+++ Soduko_Solver/Sudoku_Solver_Helper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soduko_Solver
+{
+    public class Sudoku_Solver_Helper
+    {
+        static SudokuState state;
+        public Sudoku_Solver_Helper(SudokuState state2) { 
+            state = state2;
+        }
+        
+        //Function that rolls back changes made by a failed recursion branch
+        public static void RollBack(int diff, int mode)
+        {
+            for (int j = 0; j < diff; j++)
+            {
+                var (number, index, (row, collumn)) = state.Stack.Pop();
+                RemoveSeenDigit(number, row, collumn);
+                state.Mat[row, collumn] = 0;
+                if ((mode == 0 && j != diff - 1) || mode == 1)   //If rolling back from naked singles => roll everything back
+                    state.Empties.Insert(index, (row, collumn)); //If not => do not roll the current guess.
+
+            }
+        }
+        //Function that resets the empties list
+        public static void ResetEmpties(int[,] mat)
+        {
+            //Intialize the empties list
+            state.Empties = new List<(int, int)>();
+            for (int rows = 0; rows < mat.GetLength(0); rows++)
+                for (int cols = 0; cols < mat.GetLength(0); cols++)
+                {
+                    if (state.Mat[rows, cols] + '0' < '0' || state.Mat[rows, cols] > state.Mat.GetLength(0))
+                        throw new Invalid_Character_Exception(state.Mat[rows, cols], rows + 1, cols + 1);
+                    if (state.Mat[rows, cols] == 0)
+                        state.Empties.Add((rows, cols));
+                }
+        }
+        //Function that returns a bit with all the possible values of cell (r,c)
+        public static int GetUsedValues(int r, int c)
+        {
+            int b = (r / state.BoxSize) * state.BoxSize + (c / state.BoxSize);
+            return state.BoxesBitMask[b] | state.RowsBitMask[r] | state.ColsBitMask[c];
+        }
+        //Function that returns the size of the cell's domain (amount of valid options)
+        public static int GetAmoutOfOptions(int used)
+        {
+            return state.Len - Count_Bits(used);
+        }
+        //Function that adds the number k to the bitmasks of row, col and their box
+        public static void AddSeenDigits(int k, int row, int col)
+        {
+            if (Is_Already_Placed(state.RowsBitMask[row], k))
+                throw new Duplicate_Val_In_Row(k, row + 1);
+            if (Is_Already_Placed(state.ColsBitMask[col], k))
+                throw new Duplicate_Val_In_Column(k, col + 1);
+            int b = (row / state.BoxSize) * state.BoxSize + (col / state.BoxSize);
+            if (Is_Already_Placed(state.BoxesBitMask[b], k))
+                throw new Duplicate_Val_In_Box(k, b + 1);
+            state.RowsBitMask[row] |= 1 << (k - 1);
+            state.ColsBitMask[col] |= 1 << (k - 1);
+            state.BoxesBitMask[b] |= 1 << (k - 1);
+        }
+        //Func that recives a bitmask and returns the position of the heighest turned on bit in the bitmask
+        public static int Get_Bit_Position(int mask)
+        {
+            int pos = 0;
+            while ((mask >> pos) > 1) pos++;
+            return pos + 1;
+        }
+        //Function that returns an array of the domain
+        public static int[] GetDomain(int used)
+        {
+            int[] domain = new int[GetAmoutOfOptions(used)];
+            int k = 0;
+            for (int j = 0; j < state.Len; j++)
+            {
+                if ((used & 1) == 0)
+                {
+                    domain[k] = j + 1;
+                    k++;
+                }
+                used >>= 1;
+            }
+            return domain;
+        }
+        //Function that removes the number k from the bitmasks of row, col and their box
+        public static void RemoveSeenDigit(int k, int row, int col)
+        {
+            state.RowsBitMask[row] &= ~(1 << ((k - 1)));
+            state.ColsBitMask[col] &= ~(1 << ((k - 1)));
+            state.BoxesBitMask[(row / state.BoxSize) * state.BoxSize + (col / state.BoxSize)] &= ~(1 << ((k - 1)));
+        }
+        //recieves integer n and returns the number of 1 bits in it.
+        public static int Count_Bits(int n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                n &= (n - 1);
+                count++;
+            }
+            return count;
+        }
+        //Function that checks if the nth bit is already turned on in bit
+        public static bool Is_Already_Placed(int bit, int n)
+        {
+            return (bit & (1 << (n - 1))) != 0;
+        }
+    }
+}
